ce84eb85518efc8649a1fc39284ac6b7d5e51764
usernameisuser11 <kds10111@gmail.com>
2026-09-12 15:52:05 +0900

    chore: refresh trust mockup and copy polish assets

--- public/local.js
+++ public/local.js
@@ -12,7 +12,7 @@
 
   loadScript('/local-core.js?v=20260912-ai-1')
     .then(() => loadScript('/reviews.js?v=20260912-reviews-2'))
-    .then(() => loadScript('/safety.js?v=20260912-safety-1'))
-    .then(() => loadScript('/copy-edit.js?v=20260912-copy-1'))
+    .then(() => loadScript('/safety.js?v=20260912-safety-mock-2'))
+    .then(() => loadScript('/copy-edit.js?v=20260912-copy-2'))
     .catch(error => console.error('[SMU.Link loader]', error));
 })();
