bc446fe617c61ec07cd2ef3f7d8cd49dda9ceefb
usernameisuser11 <kds10111@gmail.com>
2026-09-12 15:22:23 +0900

    feat: load SMU.Link trust and safety UI

--- public/local.js
+++ public/local.js
@@ -12,5 +12,6 @@
 
   loadScript('/local-core.js?v=20260912-ai-1')
     .then(() => loadScript('/reviews.js?v=20260912-reviews-1'))
+    .then(() => loadScript('/safety.js?v=20260912-safety-1'))
     .catch(error => console.error('[SMU.Link loader]', error));
 })();
