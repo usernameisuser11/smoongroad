e5bc19250cd1089dbe90b8da966d1c7164cd87a5
usernameisuser11 <kds10111@gmail.com>
2026-09-12 15:41:11 +0900

    chore: refresh joined-project review script

--- public/local.js
+++ public/local.js
@@ -11,7 +11,7 @@
   }
 
   loadScript('/local-core.js?v=20260912-ai-1')
-    .then(() => loadScript('/reviews.js?v=20260912-reviews-1'))
+    .then(() => loadScript('/reviews.js?v=20260912-reviews-2'))
     .then(() => loadScript('/safety.js?v=20260912-safety-1'))
     .then(() => loadScript('/copy-edit.js?v=20260912-copy-1'))
     .catch(error => console.error('[SMU.Link loader]', error));
